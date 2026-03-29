fb50bbb09666866a93d91db3b30f9ee6e05990f1
Thao198381 <157489934+Thao198381@users.noreply.github.com>
2026-03-29 11:57:45 +0700

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,6 +1,7 @@
 
 import { Topic, ExamCodeDefinition, NewsItem, FixedConfig } from './types';
-export const DEFAULT_API_URL = "https://script.google.com/macros/s/AKfycbwZdb9kvZk6acp9aVkZBvsV-4hXpnQOdHYzOS1jHFmvwPsYcjXz7IDBQ8xuF_PgB3Bkkg/exec";
+export const DEFAULT_API_URL = "https://script.google.com/macros/s/AKfycbx9Kym3rbEDC_BNHdcGLjYpzZbIQWVoBNDQ3rgmows76rONjJdoGZiQoeTpM6VSSDCdjw/exec";
+export const ADMIN_URL = "https://script.google.com/macros/s/AKfycbx9Kym3rbEDC_BNHdcGLjYpzZbIQWVoBNDQ3rgmows76rONjJdoGZiQoeTpM6VSSDCdjw/exec";
 export const SPREADSHEET_ID_ADMIN = "1LlFAI1J0b7YQ84BL674r2kr3wSoW9shgsXSIXVPDypM"; // Admin 1
 
 export const DANHGIA_URL = DEFAULT_API_URL;
